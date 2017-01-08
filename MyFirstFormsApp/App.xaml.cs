196fd421915524b2541a00076a7436afee49fb2e
Tobias Scholze <tscholze@Tobiass-MBP.fritz.box>
2017-01-08 17:26:55 +0100

    Initial commit

---
+++ MyFirstFormsApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace MyFirstFormsApp
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MyFirstFormsAppPage();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ MyFirstFormsApp/MyFirstFormsAppPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Xamarin.Forms;
+
+namespace MyFirstFormsApp
+{
+	public partial class MyFirstFormsAppPage : ContentPage
+	{
+		#region Private properties 
+
+		int tapCount;
+
+		#endregion
+
+		#region Init
+
+		public MyFirstFormsAppPage()
+		{
+			InitializeComponent();
+			UpdateControls();
+		}
+
+		#endregion
+
+		#region Event handler
+
+		void IncrementButton_Clicked(object sender, System.EventArgs e)
+		{
+			tapCount++;
+			UpdateControls();
+		}
+
+		void DecrementButton_Clicked(object sender, System.EventArgs e)
+		{
+			tapCount--;
+			UpdateControls();
+		}
+
+		#endregion
+
+		#region Private helper 
+
+		private void UpdateControls()
+		{
+			if (tapCount == 1)
+			{
+				tapLabel.Text = $"You tapped {tapCount} time.";
+			}
+			else
+			{
+				tapLabel.Text = $"You tapped {tapCount} times.";
+			}
+
+			decrementButton.IsEnabled = tapCount > 0;
+		}
+			
+		#endregion
+	}
+}
